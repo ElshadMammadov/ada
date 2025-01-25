ffc3daf476d65c1ac0f6f1f928eeadf783f6cdd0
ElshadMammadov <elshadgm@code.edu.az>
2025-01-25 19:49:35 +0400

    adasdasdasd

--- WebApplication4/Program.cs
+++ WebApplication4/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hellofsdfdfsd World!");
+app.MapGet("/", () => "Hellofsdfdfsdadsadasdasdasdd World!");
 
 app.Run();
