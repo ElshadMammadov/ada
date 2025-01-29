4d9f9f9a9db7a2f5531b4eee1156e30a1ebacf1f
ElshadMammadov <elshadgm@code.edu.az>
2025-01-29 19:53:46 +0400

    adsd

--- WebApplication4/obj/Debug/net6.0/WebApplication4.AssemblyInfo.cs
+++ WebApplication4/obj/Debug/net6.0/WebApplication4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cc2f2d7f73e3a066a80dfb5471aab98fb6648c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1881a6f50cdfdf87e208f99dd5210ab62f034347")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
